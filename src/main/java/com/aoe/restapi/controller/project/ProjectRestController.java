1ece2568d23c61e6ba53c2655c0de26af8f2d07b
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-02-28 11:04:20 +0300

    domain rest controller implemented

--- src/main/java/com/aoe/restapi/controller/project/ProjectRestController.java
+++ src/main/java/com/aoe/restapi/controller/project/ProjectRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.HashMap;
 
 public interface ProjectRestController {
+    // todo move that
     public ResponseEntity<HashMap<String, Object>> addUserToProject(int projectId, int userId);
 
     public ResponseEntity<HashMap<String, Object>> removeUserFromProject(int projectId, int userId);

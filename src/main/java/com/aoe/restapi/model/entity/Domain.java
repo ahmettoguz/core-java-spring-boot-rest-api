7453a5f91081d4a8b7bf8998938e02c9a0bf8845
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-02-28 10:49:03 +0300

    domain entity configured

--- src/main/java/com/aoe/restapi/model/entity/Domain.java
+++ src/main/java/com/aoe/restapi/model/entity/Domain.java
@@ -1,17 +1,20 @@
 package com.aoe.restapi.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.aoe.restapi.controller.base.Identifiable;
+import com.aoe.restapi.model.service.Activatable;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
+
 @Entity
 @Table(name = "domains")
-public class Domain {
+public class Domain implements Activatable, Identifiable {
     // fields
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "_id")
-    private int id;
+    private Integer id;
 
     @Column(name = "name")
     private String name;
@@ -20,25 +23,28 @@ public class Domain {
     private Boolean isActive;
 
     // relational field
+    @JsonIgnore
     @OneToOne(mappedBy = "domain", cascade = CascadeType.ALL)
-    @JsonBackReference
     private User user;
 
     // constructor
     public Domain() {
     }
 
-    public Domain(String name, Boolean isActive) {
+    public Domain(Integer id, String name, Boolean isActive, User user) {
+        this.id = id;
         this.name = name;
         this.isActive = isActive;
+        this.user = user;
     }
 
     // getter and setter
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    @Override
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -55,17 +61,24 @@ public class Domain {
         return isActive;
     }
 
+    @Override
     public void setActive(Boolean active) {
         isActive = active;
     }
 
-    // relational getter and setter
     public User getUser() {
         return user;
     }
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    // relational getter and setter
+    public Integer getUserId() {
+        if (user == null)
+            return null;
+        return user.getId();
     }
 
     // toString
@@ -75,6 +88,7 @@ public class Domain {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", isActive=" + isActive +
+                ", user=" + user +
                 '}';
     }
 }

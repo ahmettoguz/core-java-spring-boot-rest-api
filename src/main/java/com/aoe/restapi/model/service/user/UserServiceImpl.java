67534c552bf9e9186db7ee8ee8116a1507e975b0
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-02-28 10:52:03 +0300

    refactor

--- src/main/java/com/aoe/restapi/model/service/user/UserServiceImpl.java
+++ src/main/java/com/aoe/restapi/model/service/user/UserServiceImpl.java
@@ -1,24 +1,14 @@
 package com.aoe.restapi.model.service.user;
 
-import com.aoe.restapi.model.entity.Domain;
-import com.aoe.restapi.model.entity.Issue;
-import com.aoe.restapi.model.entity.Project;
 import com.aoe.restapi.model.entity.User;
 import com.aoe.restapi.model.service.base.crud.BaseCrudServiceImpl;
 import com.aoe.restapi.utility.Status.OperationStatus;
 import com.aoe.restapi.utility.Status.OperationStatusError;
 import com.aoe.restapi.utility.Status.OperationStatusSuccess;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Service
 public class UserServiceImpl<T extends User> extends BaseCrudServiceImpl<T> implements UserService<T> {

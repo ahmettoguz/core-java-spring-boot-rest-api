15cecf66e562c5a8923e6ac15d3b7651a241626e
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-03-07 15:03:01 +0300

    user rest controller is implemented

--- src/main/java/com/aoe/restapi/controller/user/UserRestController.java
+++ src/main/java/com/aoe/restapi/controller/user/UserRestController.java
@@ -1,4 +1,10 @@
 package com.aoe.restapi.controller.user;
 
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+
 public interface UserRestController {
+
+    public ResponseEntity<HashMap<String, Object>> updateUserPassword(int userId, HashMap<String, String> requestBody);
 }

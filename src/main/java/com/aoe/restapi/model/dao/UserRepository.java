38f718ecf1f5160c7333653548720ab233661b1e
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-03-08 12:31:23 +0300

    exact search by first name repository is implemented

--- src/main/java/com/aoe/restapi/model/dao/UserRepository.java
+++ src/main/java/com/aoe/restapi/model/dao/UserRepository.java
@@ -3,7 +3,11 @@ package com.aoe.restapi.model.dao;
 import com.aoe.restapi.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 // entity type and primary key
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByEmail(String email);
+
+    List<User> findByFirstName(String firstName);
 }

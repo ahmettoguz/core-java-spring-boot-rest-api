3d45a14638227ca2fc48057be743d89fa0b62b59
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-03-14 18:48:59 +0300

    security configurations is started to implement

--- src/main/java/com/aoe/restapi/security/securityConfig.java
+++ src/main/java/com/aoe/restapi/security/securityConfig.java
@@ -3,23 +3,37 @@ package com.aoe.restapi.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class securityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        // use http basic authentication
-        httpSecurity.httpBasic(Customizer.withDefaults());
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeRequests()
+                .dispatcherTypeMatchers(HttpMethod.GET).permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic(httpBasic -> {
+                });
 
-        // disable csrf (cross site request forgery), In general rest apis do not require that
-        httpSecurity.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
 
-        return httpSecurity.build();
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = User.builder()
+                .username("ahmet@hotmail.com")
+                .password("{noop}1234")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
     }
 }

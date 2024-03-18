5db3f2e9618ce334a6dde77d2fe2618e8585ae93
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-03-18 14:19:52 +0300

    auth service is removed

--- src/main/java/com/aoe/restapi/model/service/auth/AuthService.java
+++
@@ -1,7 +0,0 @@
-package com.aoe.restapi.model.service.auth;
-
-import com.aoe.restapi.utility.Status.OperationStatus;
-
-public interface AuthService {
-    OperationStatus findUserByEmail(String email);
-}

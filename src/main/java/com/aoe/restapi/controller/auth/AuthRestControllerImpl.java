d59d8b6e316589b6ea3852eff12d3d61af60cb63
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-03-18 09:13:16 +0300

    auth controller in progress

--- src/main/java/com/aoe/restapi/controller/auth/AuthRestControllerImpl.java
+++ src/main/java/com/aoe/restapi/controller/auth/AuthRestControllerImpl.java
@@ -1,9 +1,13 @@
 package com.aoe.restapi.controller.auth;
 
-import com.aoe.restapi.model.entity.User;
+import com.aoe.restapi.dto.JwtResponseDto;
+import com.aoe.restapi.dto.LoginRequestDto;
 import com.aoe.restapi.model.service.auth.AuthService;
-import com.aoe.restapi.utility.Status.OperationStatus;
+import com.aoe.restapi.utility.Status.OperationStatusError;
+import com.aoe.restapi.utility.Status.OperationStatusSuccess;
+import com.aoe.restapi.utility.auth.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,22 +18,50 @@ import java.util.HashMap;
 @RequestMapping("/auth")
 public class AuthRestControllerImpl implements AuthRestController {
 
+    private JwtUtil jwtUtil;
     AuthService authService;
 
     @Autowired
-    public AuthRestControllerImpl(AuthService authService) {
+    public AuthRestControllerImpl(JwtUtil jwtUtil, AuthService authService) {
+        this.jwtUtil = jwtUtil;
         this.authService = authService;
     }
 
-    @Override
+
+//    @Override
+//    @PostMapping("/login")
+//    public ResponseEntity<HashMap<String, Object>> login(@RequestBody(required = false) User user) {
+//        String email = user.getEmail();
+//        String password = user.getPassword();
+//
+//        // perform operation and return
+//        OperationStatus operationStatus = authService.findUserByEmail(email);
+//
+//        return operationStatus.getResponseEntity();
+//    }
+
     @PostMapping("/login")
-    public ResponseEntity<HashMap<String, Object>> login(@RequestBody(required = false) User user) {
-        String email = user.getEmail();
-        String password = user.getPassword();
-
-        // perform operation and return
-        OperationStatus operationStatus = authService.findUserByEmail(email);
-
-        return operationStatus.getResponseEntity();
+    public ResponseEntity<HashMap<String, Object>> login(@RequestBody LoginRequestDto loginRequestDto) {
+        boolean isAuthenticated = true;
+        if (isAuthenticated) {
+            // If the user is authenticated, generate a JWT token.
+            String token = jwtUtil.generateToken(loginRequestDto.getId());
+            return new OperationStatusSuccess<String>(token).getResponseEntity();
+        } else {
+            return new OperationStatusError(HttpStatus.UNAUTHORIZED).getResponseEntity();
+        }
     }
+
+//    // This endpoint is for validating the token. It can be used to check if a token is valid.
+//    @GetMapping("/validate")
+//    public ResponseEntity<?> validateToken(@RequestHeader("Authorization") String token) {
+//        String jwt = token.substring(7); // Remove "Bearer " from the token
+//        boolean isValid = jwtUtil.validateToken(jwt);
+//
+//        if (isValid) {
+//            return ResponseEntity.ok("Token is valid");
+//        } else {
+//            return ResponseEntity.status(401).body("Invalid token");
+//        }
+//    }
 }

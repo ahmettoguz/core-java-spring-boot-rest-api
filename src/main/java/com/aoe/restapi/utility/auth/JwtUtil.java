5b05e3d4924bfa7734e08d47a60152c9a332a4ca
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-03-18 08:44:11 +0300

    jwt util is added

---
+++ src/main/java/com/aoe/restapi/utility/auth/JwtUtil.java
@@ -0,0 +1,55 @@
+package com.aoe.restapi.utility.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expirationHours}")
+    private int expirationHours;
+
+    public String generateToken(Integer id) {
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expirationHours * 3600000);
+
+        return Jwts.builder()
+                .setSubject(id + "")
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}

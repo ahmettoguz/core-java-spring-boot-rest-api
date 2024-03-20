3d1434ab15a35eb80e26df125ecbb870fd088a48
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-03-20 10:20:23 +0300

    http util is added

---
+++ src/main/java/com/aoe/restapi/utility/httputil/HttpUtil.java
@@ -0,0 +1,27 @@
+package com.aoe.restapi.utility.httputil;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+public class HttpUtil {
+
+    private static String removePrecedingBearer(String token) {
+        return token.substring(7);
+    }
+
+    public static String getTokenFromHeader() {
+        // Get the current request
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+
+        // Extract the Authorization header
+        String header = request.getHeader("Authorization");
+
+        // Check if the header is not null and starts with "Bearer"
+        if (header == null || !header.startsWith("Bearer "))
+            return null;
+        // todo handle and throw good exception
+
+        return removePrecedingBearer(header);
+    }
+}

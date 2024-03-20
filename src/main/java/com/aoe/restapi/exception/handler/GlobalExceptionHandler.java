881b8cd4c4a315643ea670a221daaaf62f8e2e86
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-03-20 10:35:10 +0300

    global exception handler is configured

---
+++ src/main/java/com/aoe/restapi/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.aoe.restapi.exception.handler;
+
+
+import com.aoe.restapi.exception.exception.AuthorizationHeaderException;
+import com.aoe.restapi.exception.exception.JwtNotValidException;
+import com.aoe.restapi.utility.Status.OperationStatusError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(JwtNotValidException.class)
+    public ResponseEntity<HashMap<String, Object>> handleJwtNotValidException(JwtNotValidException ex) {
+        return new OperationStatusError(HttpStatus.UNAUTHORIZED, ex.getMessage()).getResponseEntity();
+    }
+
+    @ExceptionHandler(AuthorizationHeaderException.class)
+    public ResponseEntity<HashMap<String, Object>> handleAuthorizationHeaderException(AuthorizationHeaderException ex) {
+        return new OperationStatusError(HttpStatus.BAD_REQUEST, ex.getMessage()).getResponseEntity();
+    }
+}

70817cec788deb2d088e5692b09eca6c84300e5b
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-03-20 08:48:14 +0300

    security config update

--- src/main/java/com/aoe/restapi/security/securityConfig.java
+++ src/main/java/com/aoe/restapi/security/securityConfig.java
@@ -3,16 +3,13 @@ package com.aoe.restapi.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class securityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeRequests()
-                .anyRequest().permitAll();
+        http.csrf(csrf -> csrf.disable());
 
         return http.build();
     }

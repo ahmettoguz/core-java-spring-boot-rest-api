7737c193ef86d75a977c6b02b1af8ce4350ff71d
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-03-20 11:37:18 +0300

    base security aspect implementation with authorization implemented

--- src/main/java/com/aoe/restapi/aspect/SecurityAspect.java
+++ src/main/java/com/aoe/restapi/aspect/SecurityAspect.java
@@ -2,13 +2,11 @@ package com.aoe.restapi.aspect;
 
 import com.aoe.restapi.exception.exception.AuthorizationException;
 import com.aoe.restapi.utility.aop.AopUtil;
-import com.aoe.restapi.utility.auth.JwtUtil;
 import com.aoe.restapi.utility.facade.UtilFacade;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -17,32 +15,23 @@ import org.springframework.stereotype.Component;
 @Order(1)
 @Component
 public class SecurityAspect {
-    private JwtUtil jwtUtil;
 
-    private UtilFacade utilFacade;
-    private AopUtil aopUtil;
+    private final UtilFacade utilFacade;
+    private final AopUtil aopUtil;
 
-    public SecurityAspect(JwtUtil jwtUtil, UtilFacade utilFacade, AopUtil aopUtil) {
-        this.jwtUtil = jwtUtil;
+    @Autowired
+    public SecurityAspect(UtilFacade utilFacade, AopUtil aopUtil) {
         this.utilFacade = utilFacade;
         this.aopUtil = aopUtil;
     }
 
-    @Autowired
-
-
-    // pointcut declarations
-    @Pointcut("execution(* readAllUsers(..))")
-    private void pointcutRead() {
-    }
-
+    // ---------------------------------------------------- pointcut declarations
     // pointcut user
     @Pointcut("execution(* com.aoe.restapi.controller.user.*.updateUserPassword(..))")
     private void updateUserPassword() {
     }
 
-    // advices
-
+    // ---------------------------------------------------- advices
     // advice user
     @Before("updateUserPassword()")
     public void updateUserPassword(JoinPoint joinPoint) {
@@ -57,35 +46,5 @@ public class SecurityAspect {
 
         if (userId != targetId)
             throw new AuthorizationException();
-
-//        MethodSignature msig = (MethodSignature) joinPoint.getSignature();
-//        System.out.println("infos about aop:\nSigniture: " + msig);
-    }
-
-    @Before("pointcutRead()")
-    public void beforeReadAllUsers(JoinPoint joinPoint) {
-
-        MethodSignature msig = (MethodSignature) joinPoint.getSignature();
-
-        Object[] args = joinPoint.getArgs();
-
-        System.out.println("infos about aop:\nSigniture: " + msig);
-
-        System.out.println("Arguments:");
-
-        for (Object a : args)
-            System.out.println(a.toString());
-
-        System.out.println("@Before is running for: readAllUsers");
-//        try {
-//            throw new Exception("Execution halted by advice.");
-//        } catch (Exception e) {
-//            e.printStackTrace(); // Handle the exception as per your requirement
-//        }
-    }
-
-    @Before("execution(* common*(..))")
-    public void beforeCommonOperation() {
-        System.out.println("@Before is running for: commonOperation, security aspect");
     }
 }

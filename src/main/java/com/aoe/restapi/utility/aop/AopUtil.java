1444ea17e44e182a235ccc04593fb27e4f974171
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-03-22 08:52:27 +0300

    authentication check method is implemented

--- src/main/java/com/aoe/restapi/utility/aop/AopUtil.java
+++ src/main/java/com/aoe/restapi/utility/aop/AopUtil.java
@@ -1,8 +1,15 @@
 package com.aoe.restapi.utility.aop;
 
 import com.aoe.restapi.exception.exception.ArgException;
+import com.aoe.restapi.exception.exception.AuthenticationException;
+import com.aoe.restapi.model.entity.User;
+import com.aoe.restapi.model.entity.UserRole;
 import org.aspectj.lang.JoinPoint;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class AopUtil {
@@ -15,5 +22,21 @@ public class AopUtil {
             throw new ArgException();
 
         return args;
+    }
+
+    public static void checkAuthentication(User user, String[] roles) {
+        if (!user.getRoleSet().stream().anyMatch(role -> role.getName().equals("project manager")))
+            throw new AuthenticationException();
+
+        Set<String> userRoles = user.getRoleSet().stream()
+                .map(UserRole::getName)
+                .collect(Collectors.toSet());
+
+        boolean hasAnyRole = Arrays.stream(roles)
+                .anyMatch(userRoles::contains);
+
+        if (!hasAnyRole) {
+            throw new AuthenticationException();
+        }
     }
 }

5ac14434f05587651ac12c5bf8c3f11ffe1addda
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-03-22 14:55:46 +0300

    security aspect in progress

--- src/main/java/com/aoe/restapi/aspect/SecurityAspect.java
+++ src/main/java/com/aoe/restapi/aspect/SecurityAspect.java
@@ -1,10 +1,9 @@
 package com.aoe.restapi.aspect;
 
-import com.aoe.restapi.exception.exception.AuthorizationException;
+import com.aoe.restapi.controller.domain.DomainRestControllerImpl;
 import com.aoe.restapi.exception.exception.CommonException;
 import com.aoe.restapi.model.entity.User;
 import com.aoe.restapi.model.service.user.UserService;
-import com.aoe.restapi.utility.aop.AopUtil;
 import com.aoe.restapi.utility.facade.UtilFacade;
 import com.aoe.restapi.utility.status.OperationStatus;
 import com.aoe.restapi.utility.status.OperationStatusError;
@@ -21,113 +20,192 @@ import org.springframework.stereotype.Component;
 public class SecurityAspect {
 
     private final UtilFacade utilFacade;
-    private final AopUtil aopUtil;
     private final UserService<User> userService;
     private final AspectFacade aspectFacade;
 
     @Autowired
-    public SecurityAspect(UtilFacade utilFacade, AopUtil aopUtil, UserService<User> userService, AspectFacade aspectFacade) {
+    public SecurityAspect(UtilFacade utilFacade, UserService<User> userService, AspectFacade aspectFacade) {
         this.utilFacade = utilFacade;
-        this.aopUtil = aopUtil;
         this.userService = userService;
         this.aspectFacade = aspectFacade;
     }
 
     // ---------------------------------------------------- pointcut declarations
-    // pointcut for all except login
+    // pointcut for all endpoints except login
     @Pointcut("execution(* com.aoe.restapi.controller..*(..)) " +
             "&& !execution(* com.aoe.restapi.controller.auth.*.login(..))")
     public void order00_commonJwtAuthentication() {
     }
 
-    // pointcut for role authentications
-//    @Pointcut("execution(* com.aoe.restapi.controller.relational.userissue.*.addIssueToUser(..)) " +
-//            "|| execution(* com.aoe.restapi.controller.relational.userproject.*.addUserToProject(..)) " +
-//            "|| execution(* com.aoe.restapi.controller.relational.userproject.*.addUserToProject(..))")
-//    private void order01_projectManagerAccess() {
+    // pointcut for all endpoints
+    @Pointcut("execution(* com.aoe.restapi.controller..*(..)) " +
+            "&& !execution(* com.aoe.restapi.controller.auth.*.login(..))")
+    private void order01_genericController() {
+    }
+
+    // advice for role authentication
+    @Before("order01_genericController()")
+    public void order01_genericController(JoinPoint joinPoint) {
+        // perform jwt operations
+        int userId = utilFacade.getIdFromJwtHeader();
+
+        // read user
+        OperationStatus operationStatus = userService.readById(userId);
+
+        // check user
+        // todo will not need that
+        if (operationStatus instanceof OperationStatusError)
+            throw new CommonException();
+
+        // get user
+        User user = (User) ((OperationStatusSuccess) operationStatus).getData();
+
+
+        // check if user have project management role
+//        AopUtil.checkAuthentication(user, new String[]{"project manager"});
+
+
+        // get target of the advice
+        Object targetClass = joinPoint.getTarget();
+        String targetMethodName = joinPoint.getSignature().getName();
+
+        System.out.println("targetMethodName: " + targetMethodName);
+        System.out.println("targetClass: " + targetClass);
+
+        // restrict endpoint according to target
+        if (targetClass instanceof DomainRestControllerImpl<?>) {
+            switch (targetMethodName) {
+                case "createInstance": // POST - /domains
+                    break;
+
+                case "readInstances": // GET - /domains
+                    break;
+
+                case "readInstanceById": // GET - /domains/{id}
+                    break;
+
+                case "readAllInstancesPagedSorted": // GET - /domains/paged
+                    break;
+
+                case "count": // GET - /domains/count
+                    break;
+
+                case "mergeUpdatedInstance": // PUT - /domains/{id}
+                    aspectFacade.authorizeWithId(user, joinPoint);
+                    break;
+
+                case "deactivateInstanceById": // PATCH - /domains/${id}/deactivate
+                    aspectFacade.authorizeWithId(user, joinPoint);
+                    break;
+
+                case "activateInstanceById": // PATCH - /domains/${id}/activate
+                    aspectFacade.restrictAccess(user, joinPoint);
+                    break;
+
+                case "deleteInstance": // DELETE - /domains/${id}
+                    aspectFacade.restrictAccess(user, joinPoint);
+                    break;
+
+                default:
+                    throw new CommonException();
+            }
+        }
+    }
+
+    //
+//
+//    // pointcut user
+//    @Pointcut("execution(* com.aoe.restapi.controller.user.*.updateUserPassword(..))")
+//    private void order02_updateUserPassword() {
 //    }
-
-//    @Pointcut("execution(* com.aoe.restapi.controller.issue.*.*(..))")
-    @Pointcut("execution(* com.aoe.restapi.controller..*(..))")
-    private void order01_projectManagerAccess() {
-    }
-
-    // pointcut user
-    @Pointcut("execution(* com.aoe.restapi.controller.user.*.updateUserPassword(..))")
-    private void order02_updateUserPassword() {
-    }
-
-    // pointcut common
-    @Pointcut("execution(* com.aoe.restapi.controller.relational.userissue.*.addIssueToUser(..)) " +
-            "|| execution(* com.aoe.restapi.controller.relational.userproject.*.addUserToProject(..))")
-    private void order03_relationalBindCommon() {
-    }
-
-
-    // ---------------------------------------------------- advices
+//
+//
+//    // pointcut for role authentications
+////    @Pointcut("execution(* com.aoe.restapi.controller.relational.userissue.*.addIssueToUser(..)) " +
+////            "|| execution(* com.aoe.restapi.controller.relational.userproject.*.addUserToProject(..)) " +
+////            "|| execution(* com.aoe.restapi.controller.relational.userproject.*.addUserToProject(..))")
+////    private void order01_projectManagerAccess() {
+////    }
+//
+//
+//    // pointcut common
+//    @Pointcut("execution(* com.aoe.restapi.controller.relational.userissue.*.addIssueToUser(..)) " +
+//            "|| execution(* com.aoe.restapi.controller.relational.userproject.*.addUserToProject(..))")
+//    private void order03_relationalBindCommon() {
+//    }
+//
+//
+//    // ---------------------------------------------------- advices
     // advice for all except login
     @Before("order00_commonJwtAuthentication()")
     public void order00_commonJwtAuthentication(JoinPoint joinPoint) {
         utilFacade.commonJwtAuthentication();
     }
+//
+//    // advice for role authentication
+//    @Before("order01_projectManagerAccess()")
+//    public void order01_projectManagerAccess(JoinPoint joinPoint) {
+//        Object target = joinPoint.getTarget();
+//
+////        if (!(target instanceof IssueRestControllerImpl<?>) && !(target instanceof ProjectRest<?>)) {
+////            return;
+////        }
+//
+//        // perform jwt operations
+//        int userId = utilFacade.getIdFromJwtHeader();
+//
+//        // read user
+//        OperationStatus operationStatus = userService.readById(userId);
+//
+//        // check user
+//        // todo will not need that
+//        if (operationStatus instanceof OperationStatusError)
+//            throw new CommonException();
+//
+//        // get user
+//        User user = (User) ((OperationStatusSuccess) operationStatus).getData();
+//
+//        // check if user have project management role
+//        AopUtil.checkAuthentication(user, new String[]{"project manager"});
+//    }
+//
+//    // advice user
+//    @Before("order02_updateUserPassword()")
+//    public void order02_updateUserPassword(JoinPoint joinPoint) {
+//        // perform jwt operations
+//        int userId = utilFacade.getIdFromJwtHeader();
+//
+//        // get arguments
+//        Object[] args = aopUtil.getArguments(joinPoint);
+//
+//        // get target id from argument
+//        int targetId = Integer.parseInt(args[0].toString());
+//
+//        if (userId != targetId)
+//            throw new AuthorizationException();
+//    }
+//
+//    // advice common
+//    @Before("order03_relationalBindCommon()")
+//    public void order03_relationalBindCommon(JoinPoint joinPoint) {
+//        // perform jwt operations
+//        int userId = utilFacade.getIdFromJwtHeader();
+//
+//        // read user
+//        OperationStatus operationStatus = userService.readById(userId);
+//
+//        // check user
+//        // todo will not need that
+//        if (operationStatus instanceof OperationStatusError)
+//            throw new CommonException();
+//
+//        // get user
+//        User user = (User) ((OperationStatusSuccess) operationStatus).getData();
+//
+//        // check if user have project management role
+//        AopUtil.checkAuthentication(user, new String[]{"project manager"});
+//    }
+//
 
-    // advice for role authentication
-    @Before("order01_projectManagerAccess()")
-    public void order01_projectManagerAccess(JoinPoint joinPoint) {
-        System.out.println("hereeeeeeeeeeeeeeee");
-        // perform jwt operations
-        int userId = utilFacade.getIdFromJwtHeader();
-
-        // read user
-        OperationStatus operationStatus = userService.readById(userId);
-
-        // check user
-        // todo will not need that
-        if (operationStatus instanceof OperationStatusError)
-            throw new CommonException();
-
-        // get user
-        User user = (User) ((OperationStatusSuccess) operationStatus).getData();
-
-        // check if user have project management role
-        AopUtil.checkAuthentication(user, new String[]{"project manager"});
-    }
-
-    // advice user
-    @Before("order02_updateUserPassword()")
-    public void order02_updateUserPassword(JoinPoint joinPoint) {
-        // perform jwt operations
-        int userId = utilFacade.getIdFromJwtHeader();
-
-        // get arguments
-        Object[] args = aopUtil.getArguments(joinPoint);
-
-        // get target id from argument
-        int targetId = Integer.parseInt(args[0].toString());
-
-        if (userId != targetId)
-            throw new AuthorizationException();
-    }
-
-    // advice common
-    @Before("order03_relationalBindCommon()")
-    public void order03_relationalBindCommon(JoinPoint joinPoint) {
-        // perform jwt operations
-        int userId = utilFacade.getIdFromJwtHeader();
-
-        // read user
-        OperationStatus operationStatus = userService.readById(userId);
-
-        // check user
-        // todo will not need that
-        if (operationStatus instanceof OperationStatusError)
-            throw new CommonException();
-
-        // get user
-        User user = (User) ((OperationStatusSuccess) operationStatus).getData();
-
-        // check if user have project management role
-        AopUtil.checkAuthentication(user, new String[]{"project manager"});
-    }
 
 }

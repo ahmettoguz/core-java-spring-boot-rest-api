4502ee24a9d34c40c884cace7c32bbebf618693a
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-03-23 17:07:31 +0300

    authorization related functions implemented

--- src/main/java/com/aoe/restapi/facade/AspectFacade.java
+++ src/main/java/com/aoe/restapi/facade/AspectFacade.java
@@ -1,11 +1,14 @@
-package com.aoe.restapi.aspect;
+package com.aoe.restapi.facade;
 
 import com.aoe.restapi.exception.exception.ArgException;
-import com.aoe.restapi.exception.exception.AuthenticationException;
 import com.aoe.restapi.exception.exception.AuthorizationException;
+import com.aoe.restapi.exception.exception.JwtNotValidException;
 import com.aoe.restapi.model.entity.User;
 import com.aoe.restapi.model.entity.UserRole;
+import com.aoe.restapi.utility.auth.JwtUtil;
+import com.aoe.restapi.utility.http.HttpUtil;
 import org.aspectj.lang.JoinPoint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -14,6 +17,13 @@ import java.util.stream.Collectors;
 
 @Service
 public class AspectFacade {
+
+    private final JwtUtil jwtUtil;
+
+    @Autowired
+    public AspectFacade(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
 
     public Object[] getArguments(JoinPoint joinPoint) {
         // get the arguments
@@ -26,8 +36,25 @@ public class AspectFacade {
         return args;
     }
 
+    public void commonJwtAuthentication() {
+        // get token from header
+        String jwtToken = HttpUtil.getTokenFromHeader();
+
+        // validate jwt token
+        if (!jwtUtil.validateToken(jwtToken))
+            throw new JwtNotValidException();
+    }
+
+    public int getIdFromJwtHeader() {
+        // get token from header
+        String jwtToken = HttpUtil.getTokenFromHeader();
+
+        // get id from jwt
+        return Integer.parseInt(jwtUtil.getIdFromToken(jwtToken));
+    }
+
     public void restrictAccess(User user, JoinPoint joinPoint) {
-        this.checkAuthentication(user, new String[]{"admin"});
+        this.authorizeWithRole(user, new String[]{"admin"});
     }
 
     public void authorizeWithId(User user, JoinPoint joinPoint) {
@@ -44,16 +71,16 @@ public class AspectFacade {
             throw new AuthorizationException();
     }
 
-    public void checkAuthentication(User user, String[] roles) {
+    public void authorizeWithRole(User user, String[] allowedRoles) {
         Set<String> userRoles = user.getRoleSet().stream()
                 .map(UserRole::getName)
                 .collect(Collectors.toSet());
 
-        boolean hasAnyRole = Arrays.stream(roles)
+        boolean hasAnyRole = Arrays.stream(allowedRoles)
                 .anyMatch(userRoles::contains);
 
         if (!hasAnyRole) {
-            throw new AuthenticationException();
+            throw new AuthorizationException();
         }
     }
 }

8b514521fb3de518761730ca4408e677444ef1a5
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-04-05 16:31:02 +0300

    auth spec refactor

--- src/main/resources/test/src/spec/auth/auth.spec.ts
+++ src/main/resources/test/src/spec/auth/auth.spec.ts
@@ -2,10 +2,9 @@ const addContext = require("mochawesome/addContext");
 const axios = require("axios");
 
 const App = require("../../app/App.ts");
+const Constant = require("../../constant/Constant.ts");
 
-before(async () => {
-  
-});
+before(async () => {});
 
 describe("Authentication Tests [auth]", function () {
   it("/api/auth/validate", async function () {
@@ -22,7 +21,7 @@ describe("Authentication Tests [auth]", function () {
 
     // make request
     const response = await axios.post(
-      `${App.baseUrl}/api/auth/validate`,
+      `${Constant.baseUrl}/api/auth/validate`,
       null,
       config
     );

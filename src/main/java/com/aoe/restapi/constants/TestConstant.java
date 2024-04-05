b12886df22589a853a2cde4df234d3fbf37af020
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-04-05 16:35:40 +0300

    remove of unnecessary constant

--- src/main/java/com/aoe/restapi/constants/TestConstant.java
+++
@@ -1,8 +0,0 @@
-package com.aoe.restapi.constants;
-
-import lombok.Getter;
-
-@Getter
-public class TestConstant {
-    private String adminJwt = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNzEyMzE2NjAzLCJleHAiOjE3NDM0MjA2MDN9.NoYi1EhXhnKO0yXQULlt0nLLqb7uu5iPpWRDPcfE_I7HYMKSEuRexVIh356wYj83H3NSpNdaXRuDQX-6LB9yTg";
-}

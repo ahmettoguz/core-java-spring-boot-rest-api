8cddacf00727bcdc31211fc71392cd164e82e42b
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-04-07 01:51:51 +0300

    refactor

--- src/main/resources/test/src/spec/_health-check/health-check.spec.ts
+++ src/main/resources/test/src/spec/_health-check/health-check.spec.ts
@@ -3,7 +3,7 @@ const addContext = require("mochawesome/addContext");
 const HealthCheckFacade = require("../../facade/HealthCheckFacade.ts");
 
 describe("Health Check Tests [health-check.spec]", function () {
-  it("/api/health-check", async function () {
+  it("[GET] /api/health-check", async function () {
     // add context information
     addContext(this, "Checking server status.");
 
@@ -15,7 +15,7 @@ describe("Health Check Tests [health-check.spec]", function () {
     }
   });
 
-  it("/api/health-check/info", async function () {
+  it("[GET] /api/health-check/info", async function () {
     // add context information
     addContext(this, "Checking app informations.");
 

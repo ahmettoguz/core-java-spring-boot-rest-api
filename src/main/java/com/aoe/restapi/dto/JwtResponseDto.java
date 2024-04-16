2a0c7f47e044852b3d9801360ca876be58c781bf
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-04-16 04:02:17 +0300

    lombok is added to dto's

--- src/main/java/com/aoe/restapi/dto/JwtResponseDto.java
+++ src/main/java/com/aoe/restapi/dto/JwtResponseDto.java
@@ -1,17 +1,14 @@
 package com.aoe.restapi.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class JwtResponseDto {
     private String token;
-
-    public JwtResponseDto(String token) {
-        this.token = token;
-    }
-
-    // Getter and setter
-    public String getToken() {
-        return token;
-    }
-
     public void setToken(String token) {
         this.token = token;
     }
--- src/main/java/com/aoe/restapi/dto/LoginRequestDto.java
+++ src/main/java/com/aoe/restapi/dto/LoginRequestDto.java
@@ -1,23 +1,19 @@
 package com.aoe.restapi.dto;
 
+import com.aoe.restapi.exception.exception.InvalidInputException;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class LoginRequestDto {
     private String email;
     private String password;
 
-    public String getEmail() {
-        return email;
+    public void validateInput() {
+        if (email == null || password == null)
+            throw new InvalidInputException();
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
 }

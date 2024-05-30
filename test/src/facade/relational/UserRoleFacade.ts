2eb4f98440bebf49c128d6632d48c3a9a33381c3
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-05-30 21:30:14 +0300

    user-role test implementation

--- test/src/facade/relational/UserRoleFacade.ts
+++ test/src/facade/relational/UserRoleFacade.ts
@@ -1,11 +1,32 @@
-const CommonFacade = require("../CommonFacade.ts");
+const axios = require("axios");
 
-const entityName = "projects";
+const Constant = require("../../constant/Constant.ts");
 
 class UserRoleFacade {
-  // static async create(jwt, data) {
-  //   return await CommonFacade.create(jwt, data, entityName);
-  // }
+  static async createRelation(jwt, userId, roleId) {
+    // prepare request
+    const url = `${Constant.baseUrl}/api/users/${userId}/roles/${roleId}`;
+    const method = "post";
+    const config = {
+      method,
+      url,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: jwt,
+      },
+    };
+
+    // make request
+    const response = await axios.request(config);
+
+    // check response
+    if (response.status !== 200) throw new Error("response is not 200");
+    if (response.data === undefined)
+      throw new Error("response data is undefined");
+
+    // return data
+    return response.data;
+  }
 
   // static async delete(jwt, instanceId) {
   //   await CommonFacade.delete(jwt, instanceId, entityName);

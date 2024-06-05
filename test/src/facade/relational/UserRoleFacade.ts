30d7f410620e4ae16343bd00b4940b0e3b6cab96
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-06-05 22:26:16 +0300

    UserRoleFacade.ts implementation

--- test/src/facade/relational/UserRoleFacade.ts
+++ test/src/facade/relational/UserRoleFacade.ts
@@ -1,36 +1,71 @@
 const axios = require("axios");
 
 const Constant = require("../../constant/Constant.ts");
+const CommonUtil = require("../../util/CommonUtil.ts");
+const App = require("../../app/App.ts");
+
+const UserFacade = require("../UserFacade.ts");
+
+const {
+    AxiosService,
+    AxiosServiceBuilder,
+} = require("../../service/AxiosService.ts");
 
 class UserRoleFacade {
-  static async createRelation(jwt, userId, roleId) {
-    // prepare request
-    const url = `${Constant.baseUrl}/api/users/${userId}/roles/${roleId}`;
-    const method = "post";
-    const config = {
-      method,
-      url,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: jwt,
-      },
-    };
+    static async createRelation(targetRoleId) {
+        // create user
+        // prepare data
+        const data = {
+            firstName: `${Constant.preKey}${CommonUtil.generateRandomWord()}`,
+            email: `${Constant.preKey}${CommonUtil.generateRandomWord()}@hotmail.com`,
+            password: `${Constant.preKey}${CommonUtil.generateRandomWord()}`,
+            isActive: true,
+        };
 
-    // make request
-    const response = await axios.request(config);
+        // perform action
+        let instanceToCreate: any = {};
+        try {
+            await UserFacade.create(data, instanceToCreate);
+        } catch (error) {
+            throw error;
+        }
 
-    // check response
-    if (response.status !== 200) throw new Error("response is not 200");
-    if (response.data === undefined)
-      throw new Error("response data is undefined");
+        // prepare request
+        const url = `${Constant.baseUrl}/api/users/${instanceToCreate.id}/roles/${targetRoleId}`;
+        const method = "post";
+        const jwt = App.admin.jwt;
 
-    // return data
-    return response.data;
-  }
+        // bind created user to existing role
+        try {
+            const axiosService = new AxiosServiceBuilder()
+                .setUrl(url)
+                .setJwt(jwt)
+                .setMethod(method)
+                .build();
+            await axiosService.request();
+        } catch (e: any) {
+            throw new Error(`Axios error with code: ${e.code}`);
+        }
 
-  // static async delete(jwt, instanceId) {
-  //   await CommonFacade.delete(jwt, instanceId, entityName);
-  // }
+        // read user and check its role
+        // read instance
+        let readInstance;
+        try {
+            readInstance = await UserFacade.readWithId(
+                App.admin.jwt,
+                instanceToCreate.id
+            );
+        } catch (error) {
+            throw error;
+        }
+
+        if (!readInstance.roleIds.includes(targetRoleId))
+            throw new Error("user and role relation cannot established");
+    }
+
+    // static async delete(jwt, instanceId) {
+    //   await CommonFacade.delete(jwt, instanceId, entityName);
+    // }
 }
 
 module.exports = UserRoleFacade;

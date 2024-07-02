cec33d681f9baad8a117359081c692c0b172efb7
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-07-02 19:49:13 +0300

    User service implementation

--- test/src/service/UserService.ts
+++ test/src/service/UserService.ts
@@ -38,6 +38,55 @@ class UserService {
         return instanceToCreate;
     }
 
+    
+    // static async readAll(jwt) {
+    //     // prepare request
+    //     const url = `${Constant.baseUrl}/api/${entityName}`;
+    //     const method = "get";
+    //
+    //     const config = {
+    //         method,
+    //         url,
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Authorization: jwt,
+    //         },
+    //     };
+    //
+    //     // make request
+    //     const response = await axios.request(config);
+    //
+    //     // check response
+    //     if (response.status !== 200) throw new Error("response is not 200");
+    //     if (response.data === undefined)
+    //         throw new Error("response data is undefined");
+    //
+    //     // return response data
+    //     return response.data.data;
+    // }
+
+    static async readWithId(jwt, instanceId) {
+        // prepare request
+        const url = `${Constant.baseUrl}/api/${entityName}/${instanceId}`;
+        const method = "get";
+
+        // read instance
+        let instanceToRead;
+        try {
+            const axiosService = new AxiosServiceBuilder()
+                .setUrl(url)
+                .setMethod(method)
+                .setJwt(jwt)
+                .build();
+            const response = await axiosService.request();
+            instanceToRead = response.data.data;
+        } catch (e: any) {
+            throw new Error(`Axios error with code: ${e.code}`);
+        }
+
+        return instanceToRead;
+    }
+
 }
 
 module.exports = UserService;

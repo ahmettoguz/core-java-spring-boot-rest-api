39e8735c3c99480574816924f660930e3a893805
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-07-06 10:16:48 +0300

    user role facade implementation

--- test/src/facade/relational/UserRoleFacade.ts
+++ test/src/facade/relational/UserRoleFacade.ts
@@ -25,6 +25,32 @@ class UserRoleFacade {
         if (!readInstance.roleIds.includes(targetRoleId))
             throw new Error("user and role relation cannot established");
     }
+
+    
+    static async deleteRelation(targetRoleId, jwt) {
+        // create user
+        const user = await UserService.create();
+
+        // create relation between user and role
+        await Service.createRelation(user.id, targetRoleId, jwt);
+
+        // read user
+        let readInstance = await UserService.readWithId(jwt, user.id);
+
+        // check role relation
+        if (!readInstance.roleIds.includes(targetRoleId))
+            throw new Error("user and role relation cannot established");
+
+        // remove relation
+        await Service.deleteRelation(user.id, targetRoleId, jwt);
+
+        // read user
+        readInstance = await UserService.readWithId(jwt, user.id);
+
+        // check role relation
+        if (readInstance.roleIds.includes(targetRoleId))
+            throw new Error("user and role relation cannot removed");
+    }
 }
 
 module.exports = UserRoleFacade;

d9aa78228f320d04fd004d455543cda601168bb3
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-09-26 11:18:24 +0300

    i#49 health check refactor

--- test/src/facade/HealthCheckFacade.ts
+++ test/src/facade/HealthCheckFacade.ts
@@ -5,19 +5,11 @@ class HealthCheckFacade {
   async checkServerStatus() {
     // perform operation
     const response = await healthCheckService.checkServerStatus();
-
-    // check status
-    if (response.status !== 200)
-      throw new Error(`response code is: ${response.status}`);
   }
 
   async checkAppInformation() {
     // perform operation
     const response = await healthCheckService.checkAppInformation();
-
-    // check status
-    if (response.status !== 200)
-      throw new Error(`response code is: ${response.status}`);
 
     // todo check backend tarafında data.data yerine direkt olarak datayı dönmem lazım
     // check data field
